3d9830a3f42614681540b1a26eaf91bf34a271fc
masptj <masptj@gmail.com>
2022-05-07 18:58:53 -0300

    First commit boiler plate Gatsby masp

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <Seo title="404: Not found" />
+    <h1>404</h1>
+    <p>Ops.. nada para mostrar</p>
+  </Layout>
+)
+
+export default NotFoundPage
