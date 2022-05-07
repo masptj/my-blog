1ea6ed34b9bfe8bf46674ef551216a303824d4a6
masptj <masptj@gmail.com>
2022-05-07 19:23:14 -0300

    testing component Link from Gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "gatsby"
 
 const AboutPage = () => (
+    <>
     <h1>About Page</h1>
+
+    <ul>
+      <li> 
+        <Link to="/" >Home</Link>
+      </li>
+
+      <li>
+        <Link to="/about" activeStyle={ {color:"red"} }>About Gatsby Link</Link>
+      </li>
+    </ul>
+
+    </>
 )
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+
     <Seo title="Home" />
     <h1>Home</h1>
+
+    <ul>
+      <li> 
+        <Link to="/" activeStyle={ {color:"red"} }>Home</Link>
+      </li>
+
+      <li>
+        <Link to="/about">About Gatsby Link</Link>
+      </li>
+    </ul>
+
+
   </Layout>
 )
 
